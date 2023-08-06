3101f867b6b71ebec83eb0c2912dd0d3e64d6701
adambaialiev <adam.baialiev@gmail.com>
2023-08-06 16:46:05 +0600

    structure collection page

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return null;
+import Collection from "./components/Collection/Collection";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/Header/SearchBar/SearchBar";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <Header />
+      <SearchBar />
+      <Collection />
+    </main>
+  );
 }
