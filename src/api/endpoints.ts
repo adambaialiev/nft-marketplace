55808b7e7246262302a7e53f16a3364e755435ca
adambaialiev <adam.baialiev@gmail.com>
2023-08-08 22:00:52 +0600

    replace axios with fetch in collection page

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,3 +2,6 @@ export enum Endpoints {
   GetUploadUrl = "/nft/upload-url",
   NFT = "/nft",
 }
+
+export const buildNftCollectionEndpoint = () =>
+  `${process.env.NEXT_PUBLIC_BE_ENDPOINT}/${Endpoints.NFT}`;
--- src/app/collection/components/Collection/Collection.tsx
+++ src/app/collection/components/Collection/Collection.tsx
@@ -1,12 +1,11 @@
 import axiosInstance from "@/api/axiosInstance";
-import { Endpoints } from "@/api/endpoints";
+import { Endpoints, buildNftCollectionEndpoint } from "@/api/endpoints";
 import CollectionClientSide from "./Collection.client";
 import { NFTItem } from "@/app/types";
 
 export default async function Collection() {
-  const collection = await axiosInstance
-    .get<NFTItem[]>(Endpoints.NFT)
-    .then((res) => res.data);
+  const response = await fetch(buildNftCollectionEndpoint());
+  const collection = (await response.json()) as NFTItem[];
 
   return <CollectionClientSide collection={collection} />;
 }
