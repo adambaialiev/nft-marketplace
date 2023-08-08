6448a532aafc480726c7f3534de44201545a595f
adambaialiev <adam.baialiev@gmail.com>
2023-08-08 18:16:08 +0600

    implement NftUploadFormGuard

---
+++ src/app/admin/components/NftUploadFormGuard/NftUploadFormGuard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import useAuthStore from "@/app/_store/useAuthStore";
+import NftUploadForm from "../NftUploadForm/NftUploadForm";
+
+export default function NftUploadFormGuard() {
+  const ethAddress = useAuthStore((state) => state.address);
+  return ethAddress ? (
+    <NftUploadForm />
+  ) : (
+    <div className="p-10">
+      <span className="text-white">Please sign in to upload NFTs</span>
+    </div>
+  );
+}
